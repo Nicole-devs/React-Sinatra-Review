80cb5919c960e572c3332a00b9a745accbc55667
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-06-08 12:56:41 +0300

    user routes

--- phase-3-sinatra-react-project/app/controllers/application_controller.rb
+++ phase-3-sinatra-react-project/app/controllers/application_controller.rb
@@ -4,36 +4,36 @@ class ApplicationController < Sinatra::Base
 # Set up the routes
 
 # Show all users
-get '/users' do
-  content_type :json
-  User.all.to_json
-end
+  get '/users' do
+    content_type :json
+    User.all.to_json
+  end
 
 # Show a specific user
-get '/users/:id' do
-  content_type :json
-  User.find(params[:id]).to_json
-end
+  get '/users/:id' do
+    content_type :json
+    User.find(params[:id]).to_json
+  end
 
 # Create a new user
-post '/users' do
-  content_type :json
-  User.create(name: params[:name], email: params[:email]).to_json
-end
+  post '/users' do
+    content_type :json
+    User.create(name: params[:name], email: params[:email]).to_json
+  end
 
 # Update a specific user
-put '/users/:id' do
-  content_type :json
-  user = User.find(params[:id])
-  user.update(name: params[:name], email: params[:email])
-  user.to_json
-end
+  put '/users/:id' do
+    content_type :json
+    user = User.find(params[:id])
+    user.update(name: params[:name], email: params[:email])
+    user.to_json
+  end
 
 # Delete a specific user
-delete '/users/:id' do
-  content_type :json
-  user = User.find(params[:id])
-  user.destroy.to_json
-end
-
+  delete '/users/:id' do
+    content_type :json
+    user = User.find(params[:id])
+    user.destroy.to_json
+  end
+  
 end
--- phase-3-sinatra-react-project/db/seeds.rb
+++ phase-3-sinatra-react-project/db/seeds.rb
@@ -1,4 +1,4 @@
-puts "🌱 Seeding spices..."
+puts "🌱 Seeding fitness..."
 
 # Seed your database here
 # db/seeds.rb
@@ -9,6 +9,7 @@ User.create(name: 'Mercy', email: 'mercy@example.com')
 User.create(name: 'Pesh', email: 'pesh@example.com')
 puts "Users after seeding: #{User.count}"
 
+puts "Workouts before seeding: #{Workout.count}"
 Workout.create(name: 'Monday Workout', user_id: 1)
 Workout.create(name: 'Wednesday Workout', user_id: 1)
 Workout.create(name: 'Friday Workout', user_id: 1)
@@ -24,11 +25,14 @@ Workout.create(name: 'Thursday Workout', user_id: 3)
 Workout.create(name: 'Monday Workout', user_id: 4)
 Workout.create(name: 'Wednesday Workout', user_id: 4)
 Workout.create(name: 'Friday Workout', user_id: 4)
+puts "Workouts after seeding: #{Workout.count}"
 
+puts "Exercises before seeding: #{Exercise.count}"
 Exercise.create(name: 'Push-ups', workout_id: 1)
 Exercise.create(name: 'Sit-ups', workout_id: 2)
 Exercise.create(name: 'Squats', workout_id: 3)
 Exercise.create(name: 'Lunges', workout_id: 4)
 Exercise.create(name: 'Plank', workout_id: 5)
+puts "Exercises after seeding: #{Exercise.count}"
 
 puts "✅ Done seeding!"
