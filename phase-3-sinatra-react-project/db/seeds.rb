afe0cd4ec46a5dc1c635d22bc8c8d67aed5a158b
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-06-08 14:27:57 +0300

    workout.create update

--- phase-3-sinatra-react-project/db/seeds.rb
+++ phase-3-sinatra-react-project/db/seeds.rb
@@ -8,21 +8,22 @@ User.create([
   { name: 'Charlie', email: 'charlie@example.com' }
 ])
 
-Workout.create(name: 'Monday Workout', user_id: 1)
-Workout.create(name: 'Wednesday Workout', user_id: 1)
-Workout.create(name: 'Friday Workout', user_id: 1)
 
-Workout.create(name: 'Tuesday Workout', user_id: 2)
-Workout.create(name: 'Thursday Workout', user_id: 2)
-Workout.create(name: 'Saturday Workout', user_id: 2)
+Workout.create([
+  { name: 'Monday Workout', user_id: 1 },
+  { name: 'Wednesday Workout', user_id: 1 },
+  { name: 'Friday Workout', user_id: 1 },
+  { name: 'Tuesday Workout', user_id: 2 },
+  { name: 'Thursday Workout', user_id: 2 },
+  { name: 'Saturday Workout', user_id: 2 },
+  { name: 'Sunday Workout', user_id: 3 },
+  { name: 'Tuesday Workout', user_id: 3 },
+  { name: 'Thursday Workout', user_id: 3 },
+  { name: 'Monday Workout', user_id: 4 },
+  { name: 'Wednesday Workout', user_id: 4 },
+  { name: 'Friday Workout', user_id: 4 }
+])
 
-Workout.create(name: 'Sunday Workout', user_id: 3)
-Workout.create(name: 'Tuesday Workout', user_id: 3)
-Workout.create(name: 'Thursday Workout', user_id: 3)
-
-Workout.create(name: 'Monday Workout', user_id: 4)
-Workout.create(name: 'Wednesday Workout', user_id: 4)
-Workout.create(name: 'Friday Workout', user_id: 4)
 
 Exercise.create(name: 'Push-ups', workout_id: 1)
 Exercise.create(name: 'Sit-ups', workout_id: 2)
