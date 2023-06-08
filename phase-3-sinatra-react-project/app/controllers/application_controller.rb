536e22ea7c604e4501396bb70f66e3f209c61d1d
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-06-08 14:49:45 +0300

    migrations creation

--- phase-3-sinatra-react-project/app/controllers/application_controller.rb
+++ phase-3-sinatra-react-project/app/controllers/application_controller.rb
@@ -2,182 +2,63 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-
-  # Exercise routes
-get '/exercises' do
-  content_type :json
-  authenticate!
-  if params[:workout_id]
-    @workout = Workout.find(params[:workout_id])
-    halt 403 unless @workout.user == @current_user
-    @exercises = @workout.exercises
-  else
-    @exercises = @current_user.workouts.map(&:exercises).flatten
+  get '/' do
+    'React frontend interface
+          and 
+  Sinatra backend API!'
   end
-  @exercises.to_json
-end
-
-get '/exercises/:id' do
-  content_type :json
-  authenticate!
-  @exercise = Exercise.find(params[:id])
-  halt 403 unless @exercise.workout.user == @current_user
-  {
-    id: @exercise.id,
-    name: @exercise.name,
-    workout: {
-      id: @exercise.workout.id,
-      name: @exercise.workout.name
-    }
-  }.to_json
-end
-
-post '/exercises' do
-  content_type :json
-  authenticate!
-  @workout = Workout.find(params[:workout_id])
-  halt 403 unless @workout.user == @current_user
-  @exercise = @workout.exercises.create(params)
-  @exercise.to_json
-end
-
-put '/exercises/:id' do
-  content_type :json
-  authenticate!
-  @exercise = Exercise.find(params[:id])
-  halt 403 unless @exercise.workout.user == @current_user
-  @exercise.update(params)
-  @exercise.to_json
-end
-
-delete '/exercises/:id' do
-  content_type :json
-  authenticate!
-  @exercise = Exercise.find(params[:id])
-  halt 403 unless @exercise.workout.user == @current_user
-  @exercise.destroy
-  { success: "Exercise deleted" }.to_json
-end
-
-get '/search/exercises' do
-  content_type :json
-  authenticate!
-  @exercises = @current_user.workouts.map(&:exercises).flatten.select do |exercise|
-    exercise.name.downcase.include?(params[:query].downcase)
+  
+  # create a new workout for a user
+  post '/users/:id/workouts' do
+    user = User.find(params[:id])
+    workout = user.workouts.create(params[:workout])
+    workout.to_json
   end
-  @exercises.to_json
-end
-
-
-# User routes
-get '/users' do
-  content_type :json
-  if params[:name]
-    @users = User.where(name: params[:name])
-  else
-    @users = User.all
+  
+  # read all workouts for a user
+  get '/users/:id/workouts' do
+    user = User.find(params[:id])
+    workouts = user.workouts
+    workouts.to_json
   end
-  @users.to_json
-end
-
-get '/users/:id' do
-  content_type :json
-  @user = User.find(params[:id])
-  {
-    id: @user.id,
-    name: @user.name,
-    email: @user.email,
-    workouts: @user.workouts.map do |workout|
-      {
-        id: workout.id,
-        name: workout.name,
-        exercises: workout.exercises.map do |exercise|
-          {
-            id: exercise.id,
-            name: exercise.name
-          }
-        end
-      }
-    end
-  }.to_json
-end
-
-post '/users' do
-  content_type :json
-  @user = User.create(params)
-  @user.to_json
-end
-
-put '/users/:id' do
-  content_type :json
-  @user = User.find(params[:id])
-  @user.update(params)
-  @user.to_json
-end
-
-delete '/users/:id' do
-  content_type :json
-  @user = User.find(params[:id])
-  @user.destroy
-  { success: "User deleted" }.to_json
-end
-
-get '/search/users' do
-  content_type :json
-  @users = User.where("email LIKE ?", "%#{params[:query]}%")
-  @users.to_json
-end
-
-
-# Workout routes
-get '/workouts' do
-  content_type :json
-  authenticate!
-  if params[:date]
-    @workouts = @current_user.workouts.where(date: params[:date])
-  else
-    @workouts = @current_user.workouts
+  
+  # update an individual workout
+  put '/workouts/:id' do
+    workout = Workout.find(params[:id])
+    workout.update(params[:workout])
+    workout.to_json
   end
-  @workouts.to_json(include: :exercises)
-end
-
-get '/workouts/:id' do
-  content_type :json
-  authenticate!
-  @workout = Workout.find(params[:id])
-  halt 403 unless @workout.user == @current_user
-  {
-    id: @workout.id,
-    name: @workout.name,
-    exercises: @workout.exercises.map do |exercise|
-      {
-        id: exercise.id,
-        name: exercise.name
-      }
-    end
-  }.to_json
-end
-
-post '/workouts' do
-  content_type :json
-  authenticate!
-  @workout = @current_user.workouts.create(params)
-  @workout.to_json(include: :exercises)
-end
-
-put '/workouts/:id' do 
-# ...
-end 
-
-delete '/workouts/:id' do 
-# ...
-end 
-
-get '/search/workouts' do
-  content_type :json
-  authenticate!
-  @workouts = @current_user.workouts.where("name LIKE ?", "%#{params[:query]}%")
-  @workouts.to_json(include: :exercises)
-end
-
-end
+  
+  # delete a workout
+  delete '/workouts/:id' do
+    workout = Workout.find(params[:id])
+    workout.destroy
+  end
+  
+  # create a new exercise for a workout
+  post '/workouts/:id/exercises' do
+    workout = Workout.find(params[:id])
+    exercise = workout.exercises.create(params[:exercise])
+    exercise.to_json
+  end
+  
+  # read all exercises for a workout
+  get '/workouts/:id/exercises' do
+    workout = Workout.find(params[:id])
+    exercises = workout.exercises
+    exercises.to_json
+  end
+  
+  # update an individual exercise
+  put '/exercises/:id' do
+    exercise = Exercise.find(params[:id])
+    exercise.update(params[:exercise])
+    exercise.to_json
+  end
+  
+  # delete an exercise
+  delete '/exercises/:id' do
+    exercise = Exercise.find(params[:id])
+    exercise.destroy
+  end
+end  
