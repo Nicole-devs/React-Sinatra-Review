5a0f3426a20836ae176425f31302b06f5cc23f5c
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-06-08 15:30:36 +0300

    seeds.rb

--- phase-3-sinatra-react-project/db/seeds.rb
+++ phase-3-sinatra-react-project/db/seeds.rb
@@ -1,37 +1,32 @@
-puts "🌱 Seeding fitness..."
+puts "🌱 Seeding spices..."
 
 # Seed your database here
+# db/seeds.rb
+User.create(name: 'Josh', email: 'alice@example.com')
+User.create(name: 'Papa', email: 'bob@example.com')
+User.create(name: 'Mercy', email: 'charlie@example.com')
+User.create(name: 'Pesh', email: 'dave@example.com')
 
-User.create([
-  { name: 'Papa', email: 'alice@example.com' },
-  { name: 'Davy', email: 'bob@example.com' },
-  { name: 'Mercy', email: 'charlie@example.com' }
-  { name: 'Pesh', email: 'charlie@example.com' }
-])
+Workout.create(name: 'Monday Workout', user_id: 1)
+Workout.create(name: 'Wednesday Workout', user_id: 1)
+Workout.create(name: 'Friday Workout', user_id: 1)
 
+Workout.create(name: 'Tuesday Workout', user_id: 2)
+Workout.create(name: 'Thursday Workout', user_id: 2)
+Workout.create(name: 'Saturday Workout', user_id: 2)
 
-Workout.create([
-  { name: 'Monday Workout', user_id: 1 },
-  { name: 'Wednesday Workout', user_id: 1 },
-  { name: 'Friday Workout', user_id: 1 },
-  { name: 'Tuesday Workout', user_id: 2 },
-  { name: 'Thursday Workout', user_id: 2 },
-  { name: 'Saturday Workout', user_id: 2 },
-  { name: 'Sunday Workout', user_id: 3 },
-  { name: 'Tuesday Workout', user_id: 3 },
-  { name: 'Thursday Workout', user_id: 3 },
-  { name: 'Monday Workout', user_id: 4 },
-  { name: 'Wednesday Workout', user_id: 4 },
-  { name: 'Friday Workout', user_id: 4 }
-])
+Workout.create(name: 'Sunday Workout', user_id: 3)
+Workout.create(name: 'Tuesday Workout', user_id: 3)
+Workout.create(name: 'Thursday Workout', user_id: 3)
 
+Workout.create(name: 'Monday Workout', user_id: 4)
+Workout.create(name: 'Wednesday Workout', user_id: 4)
+Workout.create(name: 'Friday Workout', user_id: 4)
 
-Exercise.create([
-{name: 'Push-ups', workout_id: 1}
-{name: 'Sit-ups', workout_id: 2} 
-{name: 'Squats', workout_id: 3}
-{name: 'Lunges', workout_id: 4}
-{name: 'Plank', workout_id: 5}
-])
+Exercise.create(name: 'Push-ups', workout_id: 1)
+Exercise.create(name: 'Sit-ups', workout_id: 2)
+Exercise.create(name: 'Squats', workout_id: 3)
+Exercise.create(name: 'Lunges', workout_id: 4)
+Exercise.create(name: 'Plank', workout_id: 5)
 
 puts "✅ Done seeding!"
