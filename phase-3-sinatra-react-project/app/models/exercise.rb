682b58af81a7072e3a6a6f2862ac03ec7cd70bd7
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-06-08 14:10:51 +0300

    models update

--- phase-3-sinatra-react-project/app/models/exercise.rb
+++ phase-3-sinatra-react-project/app/models/exercise.rb
@@ -1,14 +1,3 @@
-class User < ActiveRecord::Base
-    has_many :workouts
-  
-    before_create :generate_pin
-  
-    private
-  
-    def generate_pin
-      loop do
-        self.pin = rand(1000..9999).to_s
-        break unless User.exists?(pin: pin)
-      end
-    end
+class Exercise < ActiveRecord::Base
+  belongs_to :workout
 end
