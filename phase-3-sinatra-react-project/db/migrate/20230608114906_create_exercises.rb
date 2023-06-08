56f8369b59456cf9e4c7540cea3169e1ed8c4484
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-06-08 14:56:38 +0300

    exercise migration

--- phase-3-sinatra-react-project/db/migrate/20230608114906_create_exercises.rb
+++ phase-3-sinatra-react-project/db/migrate/20230608114906_create_exercises.rb
@@ -1,4 +1,10 @@
 class CreateExercises < ActiveRecord::Migration[6.1]
   def change
+    create_table :exercises do |t|
+      t.string :name
+      t.belongs_to :workout, foreign_key: true
+      t.timestamps
+    end
   end
+
 end
